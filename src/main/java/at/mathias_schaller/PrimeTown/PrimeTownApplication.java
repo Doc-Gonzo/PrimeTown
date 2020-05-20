e9fdaf2c98c0668c47397712a9a2af8aeab0e81a
Mathias Schaller <stoertebecker@gmx.at>
2020-05-20 14:09:52 +0200

    Enable Auditing

--- src/main/java/at/mathias_schaller/PrimeTown/PrimeTownApplication.java
+++ src/main/java/at/mathias_schaller/PrimeTown/PrimeTownApplication.java
@@ -2,8 +2,11 @@ package at.mathias_schaller.PrimeTown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PrimeTownApplication {
 
 	public static void main(String[] args) {
