220c76c36d148e3a18c7616a9373adc326792c88
Mathias Schaller <stoertebecker@gmx.at>
2020-05-20 13:53:21 +0200

    Initial commit

---
+++ src/main/java/at/mathias_schaller/PrimeTown/PrimeTownApplication.java
@@ -0,0 +1,13 @@
+package at.mathias_schaller.PrimeTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/mathias_schaller/PrimeTown/PrimeTownApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mathias_schaller.PrimeTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
